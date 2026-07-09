5d8aea8289a1acdc20acb6a013e2f87deccdfec9
Kinetic64 <ben.ward@simplygreat.education>
2026-07-09 11:56:52 +0100

    Finished 3-2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public InputAction jumpAction;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        jumpAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(jumpAction.triggered && isOnGround)
+        {
+             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+             isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+        Debug.Log("Game Over");
+        gameOver = true;
+        isOnGround = false;
+        }
+    }
+}
